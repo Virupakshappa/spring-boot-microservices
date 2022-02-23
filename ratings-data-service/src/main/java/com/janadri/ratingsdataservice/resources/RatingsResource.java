2223c2cec8266befef43fc58c6e4668677727a66
Veeru <vjanadri7@gmail.com>
2022-02-23 23:53:32 +0530

    JP-32: Updating the branch with rest template implementation

---
+++ ratings-data-service/src/main/java/com/janadri/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,27 @@
+package com.janadri.ratingsdataservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.janadri.ratingsdataservice.model.Rating;
+import com.janadri.ratingsdataservice.model.UserRating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+
+}
