72e78cba177d627f17235eb0275f878461d83e47
Veeru <vjanadri7@gmail.com>
2022-03-22 19:00:26 +0530

    JP-32: Building Hystrix Dashboard

--- movie-catalog-service/src/main/java/com/janadri/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/janadri/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
